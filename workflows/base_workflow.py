3b37324f6211ddec17fd6c18db361ad29ff1808e
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2024-04-06 13:53:56 +1100

    Added a bracket lol

--- workflows/base_workflow.py
+++ workflows/base_workflow.py
@@ -51,8 +51,7 @@ def main():
                              cache_identifier='NLPPerCategorySentimentAndOverallSentimentWomensClothesReview',
                              use_cache=True,
                              temperature=0.2,
-                             max_validation_retries=3
-                             )
+                             max_validation_retries=3)
         handler.run()
         handler.flatten_output_data(column_names=chain.latest_column_names)
         chain.append(
