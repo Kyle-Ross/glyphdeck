1d9986213b15b07014d794c72e210ceef3028eb2
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2024-08-31 20:41:49 +1000

    Updated to match new log schema

--- CategoriGen/tools/time.py
+++ CategoriGen/tools/time.py
@@ -19,25 +19,25 @@ def delta_time_formatter(total_seconds: float) -> str:
 class RuntimeLogBlock:
     """Content manager for 'with' blocks that logs the time elapsed over the total runtime of the block"""
 
-    log_suffix = "from RuntimeLogBlock context manager class"
+    message = "in RuntimeLogBlock class"
 
-    @log_decorator(logger, start=log_suffix, finish=log_suffix)
+    @log_decorator(logger, show_nesting=False, suffix_message=message)
     def __init__(self, logger_arg: logging.Logger):
         self.logger = logger_arg
 
-    @log_decorator(logger, start=log_suffix, finish=log_suffix)
+    @log_decorator(logger, show_nesting=False, suffix_message=message)
     def __enter__(self):  # Record the time at the start of the block
         self.start_time: float = time.time()
         return self
 
-    @log_decorator(logger, start=log_suffix, finish=log_suffix)
+    @log_decorator(logger, show_nesting=False, suffix_message=message)
     def __exit__(
         self, exc_type, exc_value, exc_tb
     ):  # Compare that to the time at the end and log the result
         self.end_time: float = time.time()
         self.elapsed_time: float = self.end_time - self.start_time
         self.elapsed_time_seconds: str = format(self.elapsed_time, ",.4f")
-        self.logger.info(
-            f"Total runtime - {delta_time_formatter(self.elapsed_time)} "
-            f"({self.elapsed_time_seconds} sec)"
-        )
+        delta_time = delta_time_formatter(self.elapsed_time)
+        delta_seconds = f"{self.elapsed_time_seconds} sec"
+        message = f" | Class | RuntimeLogBlock | Finish | Total runtime | | | {delta_time} | {delta_seconds}"
+        self.logger.info(message)
