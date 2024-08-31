7f0c521bab66193d109fd1a29562942598d008d9
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2024-08-31 22:34:44 +1000

    Constant for control of whether inputs and outputs are stored in logs

--- CategoriGen/logger_constants.py
+++ CategoriGen/logger_constants.py
@@ -9,6 +9,12 @@ file_log_level_default: int = 10
 console_log_level_default: int = 10
 
 # -----------------
+# Private Information Control
+# -----------------
+log_input_data: bool = False
+log_output_data: bool = False
+
+# -----------------
 # Granular Control
 # -----------------
 
