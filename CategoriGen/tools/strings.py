ea36ec5c7d24bcdca773cff9dfff464b6a98e7cf
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2024-09-29 13:38:01 +1000

    Created all strings.py docstrings

--- CategoriGen/tools/strings.py
+++ CategoriGen/tools/strings.py
@@ -5,5 +5,12 @@ logger = StringsToolsLogger().setup()
 
 @log_decorator(logger)
 def string_cleaner(input_str: str) -> str:
-    """Basic function to clean input strings."""
+    """Cleans a given string by trimming whitespace, converting to lowercase, and removing spaces.
+
+    Args:
+        input_str: The string to be cleaned.
+
+    Returns:
+        A cleaned string which is trimmed, converted to lowercase, and has all spaces removed.
+    """
     return input_str.strip().lower().replace(" ", "")
