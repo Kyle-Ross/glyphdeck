541e1f087752ae6a37a8baa0121706ec3834be43
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2024-10-03 22:58:26 +1000

    Module level Internal object leading underscore refactor

--- glyphdeck/config/logger_levels.py
+++ glyphdeck/config/logger_levels.py
@@ -5,31 +5,31 @@ from glyphdeck.config.logger_config import access_logging_config  # noqa: F401
 # Levels - 0 NOTSET | 10 DEBUG | 20 INFO | 30 WARNING | 40 ERROR | 50 CRITICAL
 
 # Get the dictionary from 
-config = access_logging_config()
+_config = access_logging_config()
 
 # -----------------
 # Set all levels
 # -----------------
 # The set all vars can be changed in different ways by either the default or set_all conditions
-if config["setting_type"] == "default":
+if _config["setting_type"] == "default":
     set_all_file_log_level: int = 99
     set_all_console_log_level: int = 20
-if config["setting_type"] == "set_all":
-    set_all_file_log_level: int = config["set_all"]["file"]
-    set_all_console_log_level: int = config["set_all"]["console"]
+if _config["setting_type"] == "set_all":
+    set_all_file_log_level: int = _config["set_all"]["file"]
+    set_all_console_log_level: int = _config["set_all"]["console"]
 
 # -----------------
 # Private Information Control
 # -----------------
-log_input_data: bool = config["private_data"]["log_input"]
-log_output_data: bool = config["private_data"]["log_output"]
+log_input_data: bool = _config["private_data"]["log_input"]
+log_output_data: bool = _config["private_data"]["log_output"]
 
 # -----------------
 # Distributing values across loggers
 # -----------------
 
 # Use the set all value if "default" or "set_all" is the current setting type
-if config["setting_type"] in ("default", "set_all"):
+if _config["setting_type"] in ("default", "set_all"):
     # data_types.py
     data_types_file_log_level: int = set_all_file_log_level
     data_types_console_log_level: int = set_all_console_log_level
@@ -79,50 +79,50 @@ if config["setting_type"] in ("default", "set_all"):
     unhandled_errors_console_log_level: int = set_all_console_log_level
 
 # Use each individual logger's config if "granular" is the current setting type
-if config["setting_type"] == "granular":
+if _config["setting_type"] == "granular":
     # data_types.py
-    data_types_file_log_level: int = config["data_types"]["file"]
-    data_types_console_log_level: int = config["data_types"]["console"]
+    data_types_file_log_level: int = _config["data_types"]["file"]
+    data_types_console_log_level: int = _config["data_types"]["console"]
 
     # Prepper.py
-    prepper_file_log_level: int = config["prepper"]["file"]
-    prepper_console_log_level: int = config["prepper"]["console"]
+    prepper_file_log_level: int = _config["prepper"]["file"]
+    prepper_console_log_level: int = _config["prepper"]["console"]
 
     # Cascade.py
-    cascade_file_log_level: int = config["cascade"]["file"]
-    cascade_console_log_level: int = config["cascade"]["console"]
+    cascade_file_log_level: int = _config["cascade"]["file"]
+    cascade_console_log_level: int = _config["cascade"]["console"]
 
     # Sanitiser.py
-    sanitiser_file_log_level: int = config["sanitiser"]["file"]
-    sanitiser_console_log_level: int = config["sanitiser"]["console"]
+    sanitiser_file_log_level: int = _config["sanitiser"]["file"]
+    sanitiser_console_log_level: int = _config["sanitiser"]["console"]
 
     # Validators.py
-    validators_file_log_level: int = config["validators"]["file"]
-    validators_console_log_level: int = config["validators"]["console"]
+    validators_file_log_level: int = _config["validators"]["file"]
+    validators_console_log_level: int = _config["validators"]["console"]
 
     # LLMHandler.py
-    llmhandler_file_log_level: int = config["llm_handler"]["file"]
-    llmhandler_console_log_level: int = config["llm_handler"]["console"]
+    llmhandler_file_log_level: int = _config["llm_handler"]["file"]
+    llmhandler_console_log_level: int = _config["llm_handler"]["console"]
 
     # Cache.py
-    cache_file_log_level: int = config["cache"]["file"]
-    cache_console_log_level: int = config["cache"]["console"]
+    cache_file_log_level: int = _config["cache"]["file"]
+    cache_console_log_level: int = _config["cache"]["console"]
 
     # base_workflow.py
-    workflow_file_log_level: int = config["workflows"]["file"]
-    workflow_console_log_level: int = config["workflows"]["console"]
+    workflow_file_log_level: int = _config["workflows"]["file"]
+    workflow_console_log_level: int = _config["workflows"]["console"]
 
     # tools/strings.py
-    strings_file_log_level: int = config["strings"]["file"]
-    strings_console_log_level: int = config["strings"]["console"]
+    strings_file_log_level: int = _config["strings"]["file"]
+    strings_console_log_level: int = _config["strings"]["console"]
 
     # tools/time.py
-    time_file_log_level: int = config["time"]["file"]
-    time_console_log_level: int = config["time"]["console"]
+    time_file_log_level: int = _config["time"]["file"]
+    time_console_log_level: int = _config["time"]["console"]
     # tools/file_importers.py
-    file_importers_file_log_level: int = config["file_importers"]["file"]
-    file_importers_console_log_level: int = config["file_importers"]["console"]
+    file_importers_file_log_level: int = _config["file_importers"]["file"]
+    file_importers_console_log_level: int = _config["file_importers"]["console"]
 
     # Unhandled errors as used in workflow files
-    unhandled_errors_file_log_level: int = config["unhandled_errors"]["file"]
-    unhandled_errors_console_log_level: int = config["unhandled_errors"]["console"]
+    unhandled_errors_file_log_level: int = _config["unhandled_errors"]["file"]
+    unhandled_errors_console_log_level: int = _config["unhandled_errors"]["console"]
