f6afc84ef0df5a51f87224f4d88bb3834e3509f8
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2024-10-05 21:51:08 +1000

    processors package docstring refactor

--- glyphdeck/processors/__init__.py
+++ glyphdeck/processors/__init__.py
@@ -1,5 +1,4 @@
-"""
-Provides a collection of processors designed to manage, process, and sanitize data records.
+"""Provides a collection of processors designed to manage, process, and sanitize data records.
 
 This package is organized into several modules, each offering distinct functionalities for handling various data processing tasks such as sanitization, handling data with large language models, and cascading data operations.
 
