520a1b2e018f41ec140d0405a67b6712b1666252
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2024-10-07 00:40:49 +1100

    Fixed theme and code block stuff

--- docs/conf.py
+++ docs/conf.py
@@ -63,8 +63,11 @@ exclude_patterns = [
 
 # Set your HTML stuff here
 templates_path = ["_templates"]
-html_theme = "insegel"
+html_theme = "nltk_theme"
 html_static_path = ["_static"]
 html_theme_options = {"navigation_depth": 2}
-html_logo = "_static\logo.png"
+html_logo = "_static/logo.png"
 html_show_sourcelink = False
+
+# Code blocks
+highlight_language = "python3"
